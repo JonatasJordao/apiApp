90407031370b9598438358583e97ed9d03776ee1
Jônatas Henrique Jordão de Oliveira <jonatashjoliveira@gmail.com>
2022-05-19 11:11:49 +0000

    atualizaçao
    ...

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { IResposta } from './../models/IResposta.model';
+import { Injectable } from '@angular/core';
+import{HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  url = 'https://api.hgbrasil.com/weather?format=json-cors&woeid=457479';
+
+  constructor(private http: HttpClient) {}
+
+   buscarPrevisao(): Observable<IResposta> {
+    return this.http.get<IResposta>(this.url);
+
+  }
+
+  buscarIcone(slug: string){
+    const icones = {
+      storm: 'thunderstorm-outline',
+      snow: 'snow-outline',
+      hail: 'snow-outline',
+      rain: 'rainy-outline',
+      fog: 'cloud-outline',
+      clear_day: 'sunny-outline',
+      clear_night: 'moon-outline',
+      cloud: 'partly-sunny-outline',
+      cloudly_day: 'partly-sunny-outline',
+      cloudly_night : 'cloudy-night-outline',
+      none_day : 'sunny-outline',
+      none_night: 'moon-outline',
+
+    };
+    return icones[slug];
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { IResposta } from './../models/IResposta.model';
+import { WeatherService } from './../services/weather.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor() {}
+  previsao: IResposta;
 
+  constructor(public weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.buscarPrevisao();
+  }
+
+  buscarPrevisao(){
+    this.weatherService.buscarPrevisao().subscribe(respostaVeia =>{
+      this.previsao = respostaVeia;
+      console.log(respostaVeia);
+    });
+  }
+
+  buscarIcone(slug: string){
+    return this.weatherService.buscarIcone(slug);
+  }
 }
